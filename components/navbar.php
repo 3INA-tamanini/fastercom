59f48439fe973a97f8b8eb983a0a50801f9b5713
CLAUDIO GHETTA <claudio.ghetta@buonarroti.tn.it>
2026-04-15 09:47:55 +0200

    fix navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,17 +1,20 @@
 <?php 
 require_once 'components/session.php';
+require_once 'db/functions.php';
 ?>
 
 
 <nav>
     <a href="register.php">register</a>
     <a href="login.php">login</a>
-    <?php if($_SESSION['ruolo'] === 'studente'){ ?>
+    <?php if(isset($_SESSION['ruolo'])){
+        if($_SESSION['ruolo'] === 'studente'){ ?>
         <a href="dashboardStudente.php">dashboard</a>
         <a href="logout.php">Logout</a>
-    <?php } ?>
-    <?php if($_SESSION['ruolo'] === 'docente'){ ?>
+    <?php }} ?>
+    <?php if(isset($_SESSION['ruolo'])){
+        if($_SESSION['ruolo'] === 'docente'){ ?>
         <a href="dashboardDocente.php">dashboard</a>
         <a href="logout.php">Logout</a>
-    <?php } ?>
+    <?php }} ?>
 </nav>
--- login.php
+++ login.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Fastercom</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
     <?php require_once "components/navbar.php"; ?>
